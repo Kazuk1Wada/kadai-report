08f36aa0fb7a36ee52552f5c27bf11992cbf53c6
kazuki.wada <ray_of_hope_sas@yahoo.co.jp>
2019-03-12 10:12:46 +0900

    Add Report edit

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
 import models.Report;
 import utils.DBUtil;
 
@@ -40,13 +39,9 @@ public class ReportsEditServlet extends HttpServlet {
 		Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
 		em.close();
-
-		Employee login_employee = (Employee) request.getSession().getAttribute("login_employee");
-		if (login_employee.getId() == r.getEmployee().getId()) {
-			request.setAttribute("report", r);
-			request.setAttribute("_token", request.getSession().getId());
-			request.getSession().setAttribute("report_id", r.getId());
-		}
+		request.setAttribute("report", r);
+		request.setAttribute("_token", request.getSession().getId());
+		request.getSession().setAttribute("report_id", r.getId());
 
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
 		rd.forward(request, response);
